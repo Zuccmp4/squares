d9fa8410b2177ae246e5b5452b1df3e1d064995f
Zuccmp4 <aeira49@gmail.com>
2026-04-15 10:37:11 -0400

    added comments for reminder for HW

--- squares.js
+++ squares.js
@@ -1,7 +1,9 @@
+//COMMIT EACH TIME YOU DO A QUESTION FOR THE HW
+
 let squareArea = document.querySelector("#squarearea");
 
 let squareCount = parseInt(Math.random()*21)+30; // 30-50
-
+//Make variable to really big z index
 
 for (let i = 0; i<squareCount; i++){
     //make a square
@@ -10,6 +12,9 @@ for (let i = 0; i<squareCount; i++){
     square.style.left = parseInt(Math.random()*650) + "px";
     square.style.top = parseInt(Math.random()*250) + "px";
     square.style.backgroundColor = getRandomColor();
+    //I can put a onclick and a function(this)
+    // if the z index of the square selected is 
+        // already in the front remove the square
     //put it on the screen
     squareArea.append(square);
 }
@@ -26,3 +31,20 @@ function getRandomColor(){
     //return that string
     return color;
 }
+
+//Make a function to add one square
+//Make sure I set that as the onclick for the button
+
+//Make a function that changes all the colors of all the squares
+//Make sure the button calls it
+//
+function changeColors(){
+    //make an array
+    let allSquares = document.querySelectorAll(".square");
+    //For loop with index or For of loop
+    //Change the color
+}
+
+//make a variable with giant number for the z-index
+    //for when clicking on squares to bring them to the front
+
